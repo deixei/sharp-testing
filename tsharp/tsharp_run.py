102ddb22224fb83a2283e893b517b1fd732fccdf
Marcio <marcio@deixei.com>
2024-04-26 12:58:31 +0200

    run id get

--- tsharp/tsharp_run.py
+++ tsharp/tsharp_run.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 class MainRun:
 
@@ -6,11 +7,23 @@ class MainRun:
         self.run_id = os.environ.get("RUN_ID")
         self.url = os.environ.get("DX_ADO_URL")
         self.pat = os.environ.get("AZURE_DEVOPS_EXT_PAT")
+        self.project = "deixei"
     
     def execute(self):
         print(f"This is a test run {self.run_id}")
         print(f"This is the url {self.url}")
         print(f"This is the pat {self.pat}")
+
+        run = self.get_run_id()
+        print(run)
+
+    def get_run_id(self):
+        url = f"{self.url}/{self.project}/_apis/test/runs/{self.run_id}?includeDetails=True&api-version=7.1-preview.3"
+
+        response = requests.get(url, auth=('PAT', self.pat))
+        data = response.json()
+
+        return data
 
 if __name__ == "__main__":
     main = MainRun()
