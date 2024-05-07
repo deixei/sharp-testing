8ade3f59af1b5e31038a8c08a6cad8b608c13e19
Marcio Parente <67408257+deixei@users.noreply.github.com>
2024-05-07 13:17:48 +0200

    env var

--- tsharp/tsharp_constants.py
+++ tsharp/tsharp_constants.py
@@ -8,8 +8,12 @@ This module contains constants used in the tsharp module.
 
 TARGET_ADO_URL = "https://dev.azure.com/deixeicom" ## /{self.url}/
 TARGET_ADO_PROJECT = "deixei" ## /{self.project}/
+
+DEBUG_RUN_ID = "1000229"
+
 VERBOSE = True
 
+# usage: AZURE_CREDENTIAL_ENV_MAPPING.get('tenant', 'AZURE_TENANT')
 AZURE_CREDENTIAL_ENV_MAPPING = dict(
     client_id='AZURE_CLIENT_ID',
     secret='AZURE_SECRET',
@@ -23,3 +27,40 @@ ADO_ENV_MAPPING = dict(
     ado_pat='AZURE_DEVOPS_EXT_PAT',
     run_id='RUN_ID'
 )
+
+class EnviromentVariables:
+    def env_config(self, name, default=None) -> str:
+        return os.environ.get(name, default)
+
+    @property
+    def subscription_id(self) -> str:
+        return self.env_config(AZURE_CREDENTIAL_ENV_MAPPING.get('subscription_id', 'SUBSCRIPTION_ID'), '')
+
+    @property
+    def client_id(self) -> str:
+        return self.env_config(AZURE_CREDENTIAL_ENV_MAPPING.get('client_id', 'AZURE_CLIENT_ID'), '')
+    
+    @property
+    def client_secret(self) -> str:
+        return self.env_config(AZURE_CREDENTIAL_ENV_MAPPING.get('secret', 'AZURE_SECRET'), '')
+
+    @property
+    def tenant_id(self) -> str:
+        return self.env_config(AZURE_CREDENTIAL_ENV_MAPPING.get('tenant', 'AZURE_TENANT'), '')
+
+
+    @property
+    def ado_url(self) -> str:
+        return self.env_config(ADO_ENV_MAPPING.get('ado_url', 'DX_ADO_URL'), TARGET_ADO_URL)
+
+    @property
+    def ado_project(self) -> str:
+        return self.env_config(ADO_ENV_MAPPING.get('ado_project', 'AZURE_DEVOPS_EXT_PROJECT'), TARGET_ADO_PROJECT)
+    
+    @property
+    def ado_pat(self) -> str:
+        return self.env_config(ADO_ENV_MAPPING.get('ado_pat', 'AZURE_DEVOPS_EXT_PAT'), '')
+
+    @property
+    def run_id(self) -> str:
+        return self.env_config(ADO_ENV_MAPPING.get('run_id', 'RUN_ID'), DEBUG_RUN_ID)
