eb3a88c3f2c42104fe8c662e13d646acfca53878
Marcio Parente <67408257+deixei@users.noreply.github.com>
2024-05-07 11:45:35 +0200

    env mapping

--- tsharp/tsharp_constants.py
+++ tsharp/tsharp_constants.py
@@ -9,3 +9,17 @@ This module contains constants used in the tsharp module.
 TARGET_ADO_URL = "https://dev.azure.com/deixeicom" ## /{self.url}/
 TARGET_ADO_PROJECT = "deixei" ## /{self.project}/
 VERBOSE = True
+
+AZURE_CREDENTIAL_ENV_MAPPING = dict(
+    client_id='AZURE_CLIENT_ID',
+    secret='AZURE_SECRET',
+    tenant='AZURE_TENANT',
+    subscription_id='SUBSCRIPTION_ID'
+)
+
+ADO_ENV_MAPPING = dict(
+    ado_url='DX_ADO_URL',
+    ado_project='AZURE_DEVOPS_EXT_PROJECT',
+    ado_pat='AZURE_DEVOPS_EXT_PAT',
+    run_id='RUN_ID'
+)
